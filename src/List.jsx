273985d537c1806afec2dcbe83fa4696797b2773
emmeiwhite <emmeiwhite@gmail.com>
2024-10-17 12:45:02 +0530

    Component Tree: App ---> List ---> Person (mapped)

--- src/List.jsx
+++ src/List.jsx
@@ -1,22 +1,13 @@
+import Person from './Person'
 export default function List({ people }) {
   return (
     <>
-      {people.map(person => {
-        const { id, name, image, age } = person
-        return (
-          <div
-            className="person"
-            key={id}
-          >
-            <img
-              src={image}
-              alt={name}
-            />
-            <h2>{name}</h2>
-            <span>{age} years</span>
-          </div>
-        )
-      })}
+      {people.map(person => (
+        <Person
+          {...person}
+          key={person.id}
+        />
+      ))}
     </>
   )
 }
---
+++ src/Person.jsx
@@ -0,0 +1,16 @@
+export default function Person({ id, name, image, age }) {
+  return (
+    <article className="person">
+      <img
+        src={image}
+        alt={name}
+        className="img"
+      />
+
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </article>
+  )
+}
